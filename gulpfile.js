6ae5a8b18a9739d56499f3571f25e120d9041489
Fabian Stiehle <f.stiehle@gmail.com>
2016-08-29 18:43:40 +0200

    improved build procces

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     minify = require('gulp-minifier'),
     path = require('path'),
+    runSequence = require('run-sequence'),
     swPrecache = require('sw-precache');
 
 var dependencies = [
@@ -22,30 +23,37 @@ var DIST_DIR = 'dist';
 // the service worker will precache resources but won't actually serve them.
 // This allows you to test precaching behavior without worry about the cache preventing your
 // local changes from being picked up during the development cycle.
-var handleFetch = true;
+var handleFetch = false;
  
 // Gulp tasks
 // ----------------------------------------------------------------------------
-gulp.task('scripts', function () {
-    bundleApp(false);
+gulp.task('dev-scripts', function () {
+    return bundleApp(true);
+});
+
+gulp.task('prod-scripts', function () {
+    return bundleApp(false);
 });
 
 gulp.task('webserver', function() {
-  connect.server({
-    port: 8080,
-    host: 'localhost',
-    root: DIST_DIR,
-    livereload: true
-  });
+    connect.server({
+        port: 8080,
+        host: 'localhost',
+        root: DIST_DIR,
+        livereload: true
+    });
 });
- 
-gulp.task('deploy', function () {
-	bundleApp(true);
+
+gulp.task('reload', function() {
+    gulp.src('./dist/**/*.*')
+        .pipe(connect.reload());
 });
  
 // Watches for changes in the DEV_DIR
-gulp.task('watch', function () {
-	gulp.watch([DEV_DIR + '/**/*'], ['sass', 'copy-static', 'scripts']);
+gulp.task('watch', function (cb) {
+	gulp.watch([DEV_DIR + '/**/*'], function(){
+        runSequence(['dev-scripts', 'copy-static', 'sass'], 'reload', cb)
+    })
 });
 
 // Copies assets to the DIST folder
@@ -102,15 +110,14 @@ gulp.task('generate-service-worker', function(callback) {
 
 // Run this for the final build
 // Run 'minify' before the code is shiped
-gulp.task('build', ['scripts',
-                    'copy-static',
-                    'generate-service-worker',
-                    'sass']);
+gulp.task('build', function(cb) {
+    runSequence(['prod-scripts', 'copy-static', 'generate-service-worker', 'sass'], 'minify', cb);
+});          
  
 // When running 'gulp' on the terminal this task will fire.
 // It will start watching for changes in every .js file.
 // If there's a change, the task 'watch' defined above will fire.
-gulp.task('default', ['scripts',
+gulp.task('default', ['dev-scripts',
                       'copy-static',
                       'generate-service-worker',
                       'sass',
@@ -119,20 +126,18 @@ gulp.task('default', ['scripts',
  
 // Private Functions
 // ----------------------------------------------------------------------------
-function bundleApp(deploy) {
-	// Browserify will bundle all our js files together in to one and will let
-	// us use modules in the front end.
-	var appBundler = browserify({
+function bundleApp(_debug) {
+	// Browserify will bundle all our js files together in to one
+	var bundler = browserify({
     	entries: DEV_DIR + '/app.jsx',
-    	debug: false
+    	debug: _debug
   	})
+    // transform ES6 and JSX to ES5 with babelify	
+    .transform("babelify", {presets: ["es2015", "react"]})
 
-  	appBundler
-  		// transform ES6 and JSX to ES5 with babelify
-	  	.transform("babelify", {presets: ["es2015", "react"]})
+  	return bundler 		  	
 	    .bundle()
-	    .on('error',gutil.log)
+	    .on('error', gutil.log)
 	    .pipe(source('bundle.js'))
-	    .pipe(gulp.dest(DIST_DIR + '/js'))
-        .pipe(connect.reload());
+	    .pipe(gulp.dest(DIST_DIR + '/js'));
 }
